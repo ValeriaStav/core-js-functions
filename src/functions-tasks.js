7457ed469e41f878034b68fef6c25f2734e1b842
ValeriaStav <valeriiastavriadi@gmail.com>
2024-04-15 00:39:36 +0300

    feat: 5 tests passing

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -70,8 +70,8 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (num) => num ** exponent;
 }
 
 /**
@@ -87,8 +87,14 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return (x) => {
+    let result = 0;
+    args.forEach((arg, i) => {
+      result += arg * x ** (args.length - i - 1);
+    });
+    return result;
+  };
 }
 
 /**
